e80e6202a5f125b6ce8da84f127ea1e96fcdeee8
sunilRgowda186 <46446432+sunilRgowda186@users.noreply.github.com>
2020-09-14 07:58:04 +0530

    GIt REPO

---
+++ AS/src/test/java/com/as/test/DemoA.java
@@ -0,0 +1,32 @@
+package com.as.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoA 
+{
+	static
+	{
+		System.setProperty("webdriver.chrome.driver","./src/test/resources/driver/chromedriver.exe");
+	}
+	public static WebDriver driver;
+	@Test
+	public void testBrowser()
+	{
+	    driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://www.google.com");
+	}
+	@Test
+	public void testSearchBox()
+	{
+		driver.findElement(By.name("q")).sendKeys("seleniumhq",Keys.ENTER);
+		Reporter.log("TestCases Works As Per The Reuirement",true);
+		
+	}
+
+}
